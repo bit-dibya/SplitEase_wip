07db1a5594fee6820221f263a8e764f264326f54
chhotuthakur <npsdahiya@gmail.com>
2024-02-07 22:30:11 +0530

    updated one layout fragment_groups.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.splitease_wip"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.splitease_wip"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -36,4 +36,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    
 }
---
+++ app/src/main/java/com/example/splitease_wip/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.splitease_wip;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sarnava.textwriter.TextWriter;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+    }
+}
